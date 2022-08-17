1fa962fa7861b3d89824b34838b875b4b1886af3
Pedro <pedromjesusti@gmail.com>
2022-08-17 11:56:10 +0100

    login page created e routed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import LandingPage from './components/landingPage/LandingPage';
 import ApplicationRoutes from './ApplicationRoutes';
 
 function App() {
--- src/ApplicationRoutes.js
+++ src/ApplicationRoutes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './components/landingPage/LandingPage';
+import LandingPage from './components/landingPage';
+import Login from "./components/login";
 
 
 export default function ApplicationRoutes() {
@@ -9,6 +10,7 @@ export default function ApplicationRoutes() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} exact />
+                <Route path="/login" element={<Login />} />
 
             </Routes>
         </BrowserRouter>
