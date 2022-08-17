360488c6ebb6e7ca0f36a484bcfb7cc6543cc670
Pedro <pedromjesusti@gmail.com>
2022-08-17 12:15:53 +0100

    private route musicPlayer created

--- src/ApplicationRoutes.js
+++ src/ApplicationRoutes.js
@@ -2,15 +2,24 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/landingPage';
 import Login from "./components/login";
+import PrivateRoutes from './PrivateRoutes';
+import MusicPlayer from './components/musicPlayer';
 
 
 export default function ApplicationRoutes() {
+
+
+    let authenticated = false
+
 
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<LandingPage />} exact />
                 <Route path="/login" element={<Login />} />
+                <Route element={<PrivateRoutes authenticated={authenticated} />} >
+                    <Route path="/musicPlayer" element={<MusicPlayer />} />
+                </Route>
 
             </Routes>
         </BrowserRouter>
