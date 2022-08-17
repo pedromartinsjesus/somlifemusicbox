26df101466a62e9fc3dba188d7332588f33ca833
Pedro <pedromjesusti@gmail.com>
2022-08-17 13:27:10 +0100

    form now with handleLogin function and preventDefault

--- src/components/login/index.js
+++ src/components/login/index.js
@@ -5,26 +5,24 @@ import "../../assetsLogin/css/Google-Style-Login.css";
 
 export default function Login() {
 
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log("hangleLogin");
+    }
+
     return (
+        < div className="login-card" >
+            <img src="/assetsLogin/img/avatar_2x.png" className="profile-img-card" alt="profile card" />
+            <p className="profile-name-card"> </p>
 
-        <>
-            {/* link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">*/}
-
-            < div className="login-card" >
-                <img src="/assetsLogin/img/avatar_2x.png" className="profile-img-card" alt="profile card" />
-                <p className="profile-name-card"> </p>
-
-                <form className="form-signin"><span className="reauth-email"> </span>
-                    <input className="form-control" type="email" required="" placeholder="Email" autofocus="" id="inputEmail" />
-                    <input className="form-control" type="password" required="" placeholder="Senha" id="inputPassword" />
-                    <div className="checkbox"></div>
-                    <button className="btn btn-primary btn-block btn-lg btn-signin" type="submit">Entrar</button>
-
-                </form>
-
-            </div >
-
-        </>
+            <form className="form-signin"><span className="reauth-email"> </span>
+                <input className="form-control" type="email" required="" placeholder="Email" autoFocus="" id="inputEmail" />
+                <input className="form-control" type="password" required="" placeholder="Senha" id="inputPassword" />
+                <div className="checkbox"></div>
+                <button className="btn btn-primary btn-block btn-lg btn-signin" onClick={handleLogin}>Entrar</button>
+            </form>
+        </div >
     )
 
 }
