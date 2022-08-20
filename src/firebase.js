39bbf4c860f240bb393b916809b009dee83300b3
Pedro <pedromjesusti@gmail.com>
2022-08-20 02:52:22 +0100

    added singer photo

--- src/firebase.js
+++ src/firebase.js
@@ -10,13 +10,13 @@ import { getAuth, onAuthStateChanged } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCvdTKv1bnEQqVJqY1_o-RZu3-CuEc2M_0",
-    authDomain: "somlife-fc4a3.firebaseapp.com",
-    projectId: "somlife-fc4a3",
-    storageBucket: "somlife-fc4a3.appspot.com",
-    messagingSenderId: "293485348087",
-    appId: "1:293485348087:web:7b474d3812bb0da311fff2",
-    measurementId: "G-40TCFGBL8K"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
 // Initialize Firebase
@@ -30,25 +30,6 @@ export const appConfig = initializeApp(firebaseConfig);
 export const db = getFirestore();
 
 
-/* export function checkUser() {
-    const auth = getAuth(appConfig)
-    let userApp;
-
-    onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User is signed in, see docs for a list of available properties
-            // https://firebase.google.com/docs/reference/js/firebase.User
-            userApp = user.displayName;
-            // ...
-        } else {
-            // User is signed out
-            // ...
-        }
-    })
-
-
-    return userApp ? userApp : "NADA"
-} */
 
 
 
